d32e09eabfb16f9c83a78096723f33741fb97c8d
desarrollo3 <desarrollo3@xpd.mx>
2024-07-23 17:13:51 -0600

    [FT-MAQUETA] conexion a base de datos y creacion de tabla

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -1,16 +1,43 @@
 'use client'
 import MiniDrawer from "@/components/pageDrawer"
 import { usePathname } from "next/navigation"
+import { supabase } from "../supabase/configSupabase"
+import { useEffect, useState } from "react"
+import { Stack, Typography } from "@mui/material"
+import TableOrdens from "@/components/tableOrder"
 
 
 
 const page = () => {
     const pathname = usePathname()
-    
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data, error } = await supabase
+                .from('orden')
+                .select()
+            
+            if (error) {
+                setError(error)
+            } else {
+                setData(data)
+            }
+            }
+            
+            fetchData()
+    }, [])
+
     return (
         <MiniDrawer>
-            
-            {pathname}
+            <Stack spacing={2}>
+                <Typography variant="h5" >Orders </Typography>
+                {data.length> 0 ?
+                    <TableOrdens row={data} />
+                    :
+                    <p> no se encontraron datos</p>
+                }
+            </Stack>
         </MiniDrawer>
     )
 }
